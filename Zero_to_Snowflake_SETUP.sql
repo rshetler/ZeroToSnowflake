2715b4ad3920aca6d8386d793db87b6c3a8884f9
Rodney Shetler <rodney.shetler@live.com>
2019-07-26 15:14:27 -0400

    SETUP script update

--- Zero_to_Snowflake_SETUP.sql
+++ Zero_to_Snowflake_SETUP.sql
@@ -63,32 +63,6 @@ from (select $1, convert_timezone('UTC', 'US/Eastern', $1:time::timestamp_ntz)
         from @citibike_s3_stage/weather/) file_format = (type = json) ;  
           
 -- Suspend the cluster you just created   
-alter warehouse load_wh suspend;  
-  
-  
--- create a view that joins the information about a specific trip to the weather info  
-create or replace  view trip_weather_vw as  
-select *  
-from trips  
-  left outer join  
-  ( select t as observation_time  
-    ,v:city.id::int as city_id  
-    ,v:city.name::string as city_name  
-    ,v:city.country::string as country  
-    ,v:city.coord.lat::float as city_lat  
-    ,v:city.coord.lon::float as city_lon  
-    ,v:clouds.all::int as clouds  
-    ,(v:main.temp::float)-273.15 as temp_avg  
-    ,(v:main.temp_min::float)-273.15 as temp_min  
-    ,(v:main.temp_max::float)-273.15 as temp_max  
-    ,v:weather[0].id::int as weather_id  
-    ,v:weather[0].main::string as weather  
-    ,v:weather[0].description::string as weather_desc  
-    ,v:weather[0].icon::string as weather_icon  
-    ,v:wind.deg::float as wind_dir  
-    ,v:wind.speed::float as wind_speed  
-   from weather  
-   where city_id = 5128638)  
-  on date_trunc(HOUR, starttime) = date_trunc(HOUR, observation_time);  
+alter warehouse load_wh suspend;   
     
 -- Once finished move to second worksheet 
